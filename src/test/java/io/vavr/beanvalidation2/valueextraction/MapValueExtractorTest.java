afddc89cefb39cee0afc3e01aaaa7c2c82c51a7d
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2019-07-17 12:35:33 +0200

    Tests to assert extraction of Vavr's Map keys & values
    This commit tests issue #2

---
+++ src/test/java/io/vavr/beanvalidation2/valueextraction/MapValueExtractorTest.java
@@ -0,0 +1,141 @@
+/*  __    __  __  __    __  ___
+ * \  \  /  /    \  \  /  /  __/
+ *  \  \/  /  /\  \  \/  /  /
+ *   \____/__/  \__\____/__/
+ *
+ * Copyright 2014-2019 Vavr, http://vavr.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.vavr.beanvalidation2.valueextraction;
+
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Path;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MapValueExtractorTest {
+
+    protected static final String SINGLE_CHAR = "^[a-z]$";
+    private Validator validator;
+
+    @Before
+    public void setUp() {
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    private <T> void validateAndAssertNoViolations(T target) {
+        Collection<ConstraintViolation<T>> violations = validator.validate(target);
+        assertThat(violations).isEmpty();
+    }
+
+    private <T> void validateAndAssertSingleViolation(T target, Class<?> constraint) {
+        Collection<ConstraintViolation<T>> violations = validator.validate(target);
+
+        assertThat(violations).isNotEmpty().hasSize(1);
+
+        ConstraintViolation<T> violation = violations.iterator().next();
+        assertThat(violation.getPropertyPath()).isNotEmpty().hasSize(2);
+
+        assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(constraint);
+
+        Iterator<Path.Node> iterator = violation.getPropertyPath().iterator();
+
+        Path.Node parent = iterator.next();
+        assertThat(parent.getName()).isEqualToIgnoringCase("map");
+    }
+
+    /**
+     * @see <a href="https://github.com/vavr-io/vavr-beanvalidation2/issues/2">GH-2</a>
+     */
+    @Test
+    @Ignore("GH-2 Not yet supported!")
+    public void defaultConstructionShouldValidate() {
+        validateAndAssertNoViolations(new TestBean());
+    }
+
+    /**
+     * @see <a href="https://github.com/vavr-io/vavr-beanvalidation2/issues/2">GH-2</a>
+     */
+    @Test
+    @Ignore("GH-2 Not yet supported!")
+    public void havingEmptyValueShouldNotValidate() {
+        TestBean bean = new TestBean();
+        bean.put("b", "");
+        validateAndAssertSingleViolation(bean, NotBlank.class);
+    }
+
+    /**
+     * @see <a href="https://github.com/vavr-io/vavr-beanvalidation2/issues/2">GH-2</a>
+     */
+    @Test
+    @Ignore("GH-2 Not yet supported!")
+    public void havingComplexKeyShouldNotValidate() {
+        TestBean bean = new TestBean();
+        bean.put("bad", "ok");
+        validateAndAssertSingleViolation(bean, Pattern.class);
+    }
+
+    @Test
+    public void jvmDefaultConstructionShouldValidate() {
+        validateAndAssertNoViolations(new JvmTestBean());
+    }
+
+    @Test
+    public void jvmHavingEmptyValueShouldNotValidate() {
+        JvmTestBean bean = new JvmTestBean();
+        bean.put("b", "");
+        validateAndAssertSingleViolation(bean, NotBlank.class);
+    }
+
+    @Test
+    public void jvmHavingComplexKeyShouldNotValidate() {
+        JvmTestBean bean = new JvmTestBean();
+        bean.put("bad", "ok");
+        validateAndAssertSingleViolation(bean, Pattern.class);
+    }
+
+    private static class TestBean {
+        private Map<@Pattern(regexp = SINGLE_CHAR) String, @NotBlank String> map =
+                HashMap.of("a", "b");
+
+        void put(String key, String value) {
+            map = map.put(key, value);
+        }
+    }
+
+    private static class JvmTestBean {
+        private java.util.Map<@Pattern(regexp = SINGLE_CHAR) String, @NotBlank String> map =
+                new java.util.HashMap<>();
+
+        JvmTestBean() {
+            put("a", "b");
+        }
+
+        void put(String key, String value) {
+            map.put(key, value);
+        }
+    }
+}
---
+++ src/test/java/io/vavr/beanvalidation2/valueextraction/SeqValueExtractorTest.java
@@ -0,0 +1,86 @@
+/*  __    __  __  __    __  ___
+ * \  \  /  /    \  \  /  /  __/
+ *  \  \/  /  /\  \  \/  /  /
+ *   \____/__/  \__\____/__/
+ *
+ * Copyright 2014-2019 Vavr, http://vavr.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.vavr.beanvalidation2.valueextraction;
+
+import io.vavr.collection.List;
+import io.vavr.collection.Seq;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Path;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SeqValueExtractorTest {
+
+    private Validator validator;
+
+    @Before
+    public void setUp() {
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    private <T> void validateAndAssertNoViolations(T target) {
+        Collection<ConstraintViolation<T>> violations = validator.validate(target);
+        assertThat(violations).isEmpty();
+    }
+
+    private <T> void validateAndAssertSingleViolation(T target) {
+        Collection<ConstraintViolation<T>> violations = validator.validate(target);
+
+        assertThat(violations).isNotEmpty().hasSize(1);
+
+        ConstraintViolation<T> violation = violations.iterator().next();
+        assertThat(violation.getPropertyPath()).isNotEmpty().hasSize(2);
+
+        assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(NotBlank.class);
+
+        Iterator<Path.Node> iterator = violation.getPropertyPath().iterator();
+
+        Path.Node parent = iterator.next();
+        assertThat(parent.getName()).isEqualToIgnoringCase("list");
+    }
+
+    @Test
+    public void defaultConstructionShouldValidate() {
+        validateAndAssertNoViolations(new TestBean());
+    }
+
+    @Test
+    public void havingEmptyValueShouldNotValidate() {
+        TestBean bean = new TestBean();
+        bean.add("");
+        validateAndAssertSingleViolation(bean);
+    }
+
+    private static class TestBean {
+        private Seq<@NotBlank String> list = List.of("a", "b");
+
+        void add(String value) {
+            list = list.append(value);
+        }
+    }
+}
